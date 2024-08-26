354c9b899f220e54e82c9e2153c2f7d2fb84c6f8
Shubham Pal <shubhampal31102003@gmail.com>
2024-08-26 15:02:04 +0530

    Converting into JSON

--- linkNova/src/linkNova/LinkNova.java
+++ linkNova/src/linkNova/LinkNova.java
@@ -8,21 +8,15 @@ public class LinkNova {
 	
 	public static void main(String[] args) {
 		
-		// Let's create some blocks using the syntax
-		// Block(String data,String previousHash)
+		//add our blocks to the blockchain ArrayList:
+		blockchain.add(new Block("Hi im the first block", "0"));		
+		blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash)); 
+		blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
 		
-		// The first block is called the genesis block, 
-		// and because there is no previous block we will just enter “0” as the previous hash.
-		Block genesisBlock = new Block("This is the first block","0");
-		System.out.println("Hash for Block 1: "+genesisBlock.hash);
-		
-		// Second Block
-		Block secondBlock = new Block("This is the second block", genesisBlock.hash);
-		System.out.println("Hash for Block 2: "+secondBlock.hash);
-		
-		// Third Block
-		Block thirdBlock = new Block("This is the third block", secondBlock.hash);
-		System.out.println("Hash for Block 3: "+thirdBlock.hash);
+		// this below line converts the block chain object into a nicely formatted 
+		// (pretty-printed) JSON string and assigns it to the blockchainJson variable.
+		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
+		System.out.println(blockchainJson);
 		
 	}
 
